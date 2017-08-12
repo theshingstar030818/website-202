4d4822a27fb2f09c6065672793bceb8899a2a2cd
Tanzeel <tanzeelrana@cmail.carleton.ca>
2017-08-12 16:29:00 -0400

    add tenant cloud code

--- web/parse_cloud/main.js
+++ web/parse_cloud/main.js
@@ -1,3 +1,34 @@
+
+var userHasRole = function(user, roleName) {
+  return new Promise((resolve, reject) => {
+    var query = new Parse.Query(Parse.Role);
+    query.equalTo("name", roleName);
+    query.equalTo("users", user);
+    query.find().then((roles)=> {
+        resolve(roles.length > 0);
+    }).catch((error)=>{
+      reject(error);
+    });
+  });      
+}
+
+var addTenant = function(request) {
+  console.log('add tenant request : ');
+  console.log(request);
+}
+
+Parse.Cloud.define('addTenant', function(request, response){
+  if(!Parse.User.current() && userHasRole(Parse.User.current(), 'super')){
+    response.error('Unautherized user');
+  }else {
+    addTenant(request).then((tenant)=>{
+      response.success(tenant);
+    }).catch((error)=>{
+      response.error(error);
+    });
+  }
+});
+
 Parse.Cloud.define('getUserRole', function(request, response){
   
   if(!Parse.User.current()){
@@ -26,7 +57,7 @@ Parse.Cloud.define('hasRole', function(request, response){
     response.error('Request did not have an authenticated user attached with it');
   }
   else {
-    userHasRole(request.params.parseSessionToken, request.params.role)
+    usernameHasRole(request.params.parseSessionToken, request.params.role)
       .then(function(hasRole){
         response.success({hasRole: hasRole});
       },
@@ -37,7 +68,7 @@ Parse.Cloud.define('hasRole', function(request, response){
   }
 });
 
-var userHasRole = function(username, rolename) {
+var usernameHasRole = function(username, rolename) {
   Parse.Cloud.useMasterKey();
   var queryRole = new Parse.Query(Parse.Role);
   queryRole.equalTo('name', rolename);
