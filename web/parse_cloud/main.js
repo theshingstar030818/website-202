9d8115f28d7addc6e94e3c5e455f5957c2e12d41
Tanzeel <tanzeelrana@cmail.carleton.ca>
2017-08-26 23:57:02 -0400

    parse cloud file update

--- web/parse_cloud/main.js
+++ web/parse_cloud/main.js
@@ -364,10 +364,12 @@ var setNewClientACLRolesAndPermissions = function(user, client, tenant){
     acl.setRoleReadAccess('super', true);
 
     // Users 
-    acl.setWriteAccess( tenant.id, true);
-    acl.setReadAccess( tenant.id, true);
-    acl.setWriteAccess( user.id, true);
-    acl.setReadAccess( user.id, true);
+    acl.setRoleReadAccess( tenant.id, true);
+    acl.setRoleWriteAccess( tenant.id, true);
+    acl.setRoleReadAccess( user.id, true);
+    acl.setRoleWriteAccess( user.id, true);
+
+    console.log("client.setACL(acl)");
 
     client.setACL(acl);
     user.setACL(acl);
